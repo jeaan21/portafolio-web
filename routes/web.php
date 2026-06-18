97b52d15fd3777db42db611bc3fb9438cc4dbcdc
jemy-22 <jariasat@undac.edu.pe>
2026-06-18 15:51:56 -0500

    debug: add /debug-log route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::post('/api/v1/post_mensaje.php', [ContactController::class, 'handle'])->m
 
 // Redirección heredada de sobre-mi.php
 Route::redirect('/sobre-mi', '/#about');
+
+Route::get('/debug-log', function() {
+    $logFile = storage_path('logs/laravel.log');
+    if (file_exists($logFile)) {
+        return response(file_get_contents($logFile), 200, ['Content-Type' => 'text/plain']);
+    }
+    return 'Log file not found';
+});
